cb09f21f5d01b81b0f69add2f939ee80a36d5c73
applemuncy <j.apple.muncy@gmail.com>
2015-11-22 21:57:04 -0500

    Edits up to page 145 and runs ok

---
+++ README.md
@@ -0,0 +1,10 @@
+CriminalIntent
+==============
+
+
+Chaptewr 7 work in progress
+from
+
+
+2nd Edition "Android Programming"
+The Big Nerd Ranch Guide
---
+++ app/src/main/java/org/bloominglabs/apple/criminalintent/Crime.java
@@ -0,0 +1,32 @@
+package org.bloominglabs.apple.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by apple on 11/22/15.
+ */
+
+
+public class Crime {
+
+    private UUID    mId;
+    private String mTitle;
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public Crime (){
+        //generate unique identifier
+        mId = UUID.randomUUID();
+    }
+
+}
