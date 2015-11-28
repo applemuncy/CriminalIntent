81477f354df32c9e47bc243cbc5a2172851ab3b3
applemuncy <j.apple.muncy@gmail.com>
2015-11-28 18:47:24 -0500

    up to page 207

---
+++ app/src/main/java/org/bloominglabs/apple/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,20 @@
+package org.bloominglabs.apple.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by apple on 11/28/15.
+ *
+ */
+public class CrimePagerActivity extends FragmentActivity {
+
+//    public void CrimePagerActivity() {}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView (R.layout.activity_crime_pager);
+
+    }
+}
